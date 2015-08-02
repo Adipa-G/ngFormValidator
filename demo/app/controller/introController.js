d1ef9ba0794aa1e4a8162d81e3616e297690176d
adipa <adipa.achintha@gmail.com>
2015-08-02 14:12:07 +1000

    removed vs solution

--- demo/app/controller/introController.js
+++
@@ -1,5 +0,0 @@
-﻿(function() {
-	angular.module('validatorApp').controller('intro',['$scope',function($scope) {
-		$scope.model = { requiredField : '' };
-	}]);
-})();
--- demo/app/controller/stringLengthController.js
+++
@@ -1,5 +0,0 @@
-﻿(function () {
-    angular.module('validatorApp').controller('stringLength', ['$scope', function ($scope) {
-        $scope.model = { field2To3: '', field2To3CustomMsg : '' };
-    }]);
-})();
